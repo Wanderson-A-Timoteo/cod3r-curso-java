1051788ecb81eb45c5af81e432813ee37afffdb2
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2022-10-01 16:14:39 -0400

    Exemplo de comentários e JAVA DOC para iniciar a documentação do projeto

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -1,8 +1,27 @@
 package fundamentos;
+//Exemplo de comentário Java Doc para iniciar uma documentação do projeto /** enter 
+/**
+ * Podemos ver as opções do JAVA Doc chamando com @
+ * @author wanderson
+ * @version 1.0
+ */
 
 public class PrimeiroPrograma {
+	
+	/**
+	 * 
+	 * @param args
+	 */
 
 	public static void main(String[] args) {
+		// Comentário de uma linha
 		System.out.println("Hello World!");
+		
+		/**
+		 * Comentários 
+		 * de
+		 * Multiplas 
+		 * Linhas
+		 * */
 	}
 }
